42903f665f4c022809afa162baeb5b4aaf31eccb
Filip Szuba <admin@notejoe.net>
2020-04-19 14:39:05 +0200

    Update HeadCommand.java

--- src/net/notejoe/HeadPlugin/commands/HeadCommand.java
+++ src/net/notejoe/HeadPlugin/commands/HeadCommand.java
@@ -14,41 +14,42 @@ public class HeadCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-	
-		if(!sender.hasPermission("notejoe.head")) {
-			sender.sendMessage(HeadPlugin.no_perms);
-		}else{
-			if(args.length == 0) {
+
+	if(!sender.hasPermission("notejoe.head")) {
+		sender.sendMessage(HeadPlugin.no_perms);
+	}else{
+		if(args.length == 0) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage(HeadPlugin.no_player);
+				return true;
+			}
+			Player player = (Player) sender;
+			ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
+			SkullMeta meta = (SkullMeta) item.getItemMeta();
+			meta.setOwner(player.getName());
+			item.setItemMeta(meta);
+					
+			player.getInventory().addItem(new ItemStack[] {item});
+			player.sendMessage("Â§6Glowa gracza Â§c" + player.getName() + " Â§6zostala dodana do twojego ekwipunku!");
+				
+			}else if(args.length == 1) {
 				if(!(sender instanceof Player)) {
 					sender.sendMessage(HeadPlugin.no_player);
 					return true;
-					}
+				}
 					Player player = (Player) sender;
+					String otherPlayer = args[0];
 					ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
 					SkullMeta meta = (SkullMeta) item.getItemMeta();
-					meta.setOwner(player.getName());
+					meta.setOwner(otherPlayer);
 					item.setItemMeta(meta);
-					
-					player.getInventory().addItem(new ItemStack[] {item});
-					player.sendMessage("§6Glowa gracza §c" + player.getName() + " §6zostala dodana do twojego ekwipunku!");
-					}else if(args.length == 1) {
-						if(!(sender instanceof Player)) {
-							sender.sendMessage(HeadPlugin.no_player);
-							return true;
-						}
-							Player player = (Player) sender;
-							String otherPlayer = args[0];
-							ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
-							SkullMeta meta = (SkullMeta) item.getItemMeta();
-							meta.setOwner(otherPlayer);
-							item.setItemMeta(meta);
 							
-							player.getInventory().addItem(new ItemStack[] {item});
-							player.sendMessage("§6Glowa gracza §c" + otherPlayer + " §6zostala dodana do twojego ekwipunku!");
-						}else{
-							sender.sendMessage("§cPoprawne uzycie: /head <nick>");
-						}
+					player.getInventory().addItem(new ItemStack[] {item});
+					player.sendMessage("Â§6Glowa gracza Â§c" + otherPlayer + " Â§6zostala dodana do twojego ekwipunku!");
+				}else{
+					sender.sendMessage("Â§cPoprawne uzycie: /head <nick>");
+				}
 		}
-		return false;	
+	return false;	
 	}
 }
